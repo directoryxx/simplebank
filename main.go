e7538b9c3b8186bac045ff85ea34ea6a1219a8ff
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2022-08-30 09:55:38 +0000

    add port

--- main.go
+++ main.go
@@ -13,5 +13,5 @@ func main() {
 			"message": "coba",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":6060") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
